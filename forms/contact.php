8723c3485c56be46f06edc03b73cafe9e7390c95
Developer-Faith <50043261+faithchelangat@users.noreply.github.com>
2025-02-12 09:55:04 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -18,11 +18,11 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
 
-
+/*
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject']; 
+  $contact->subject = $_POST['subject']; */
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -44,4 +44,4 @@ if ($contact) {
     } else {
         echo "Error: Failed to send message.";
     }
-?>
+?> 
