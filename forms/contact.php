01684fe5768acddf01b17b74f6dfbb0adf86a7c1
Developer-Faith <50043261+faithchelangat@users.noreply.github.com>
2025-02-12 09:46:23 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -18,11 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
 
- if ($contact) {
-        echo "Message sent successfully!";
-    } else {
-        echo "Error: Failed to send message.";
-    }
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -38,9 +34,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+  /*$contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['message'], 'Message', 10);*/
 
   //echo $contact->send();
 if ($contact) {
